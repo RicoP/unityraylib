cf6fda7ebea280fdfbc6231979af411184d69f40
RicoP <RicoP@users.noreply.github.com>
2025-02-10 11:26:09 +0100

    more fixes

--- src/unityraylib.c
+++ src/unityraylib.c
@@ -16,10 +16,39 @@
 #endif
 
 #if defined(_WIN32)
-    #if !defined(WIN32_LEAN_AND_MEAN)
-        #define WIN32_LEAN_AND_MEAN
-    #endif
-    #include <windows.h>
+#   if !defined(WIN32_LEAN_AND_MEAN)
+#        define WIN32_LEAN_AND_MEAN
+#   endif
+
+//from https://github.com/raylib-extras/extras-c/blob/main/raylib_win32.h
+// move the windows functions to new names
+// note that you can't call these functions or structures from your code, but you should not neeed to
+#define CloseWindow CloseWindowWin32
+#define Rectangle RectangleWin32
+#define ShowCursor ShowCursorWin32
+#define LoadImageA LoadImageAWin32
+#define LoadImageW LoadImageWin32
+#define DrawTextA DrawTextAWin32
+#define DrawTextW DrawTextWin32
+#define DrawTextExA DrawTextExAWin32
+#define DrawTextExW DrawTextExWin32
+
+// include windows
+#include <windows.h>
+
+// remove all our redfintions so that raylib can define them properly
+#undef CloseWindow
+#undef Rectangle
+#undef ShowCursor
+#undef LoadImage 
+#undef LoadImageA
+#undef LoadImageW
+#undef DrawText 
+#undef DrawTextA
+#undef DrawTextW
+#undef DrawTextEx 
+#undef DrawTextExA
+#undef DrawTextExW
 #endif
 
 #include "raylib.h"
