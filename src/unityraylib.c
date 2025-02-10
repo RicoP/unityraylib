b5203699954047908c9c0fdabe3100267863fa44
RicoP <RicoP@users.noreply.github.com>
2025-02-10 13:07:16 +0100

    reorder include order

--- src/unityraylib.c
+++ src/unityraylib.c
@@ -8,12 +8,110 @@
 #endif
 
 #if defined(__linux__)
+    #define _GNU_SOURCE
+    #include <poll.h> // for ppoll
+
     #if !defined(_GLFW_WAYLAND) && !defined(_GLFW_X11)
         #define _GLFW_X11
     #endif
-    #define _GNU_SOURCE
-    #include <poll.h>
+    #if defined(_GLFW_X11)
+        #define Font Font_
+        #include <X11/Xlib.h>
+        #include <X11/Xcursor/Xcursor.h>
+        #include <linux/input.h>
+        #undef Font
+        // linux/input-event-codes.h defins a lot of key codes that conflict with raylib
+        #undef KEY_APOSTROPHE
+        #undef KEY_CAPS_LOCK
+        #undef KEY_CAPSLOCK
+        #undef KEY_ESCAPE
+        #undef KEY_KP_ADD
+        #undef KEY_KP_SUBTRACT
+        #undef KEY_LEFT_CONTROL
+        #undef KEY_LEFTCTRL
+        #undef KEY_NUM_LOCK
+        #undef KEY_NUMLOCK
+        #undef KEY_PLAY
+        #undef KEY_SCALE
+        #undef KEY_CAPS_LOCK
+        #undef KEY_COMMA
+        #undef KEY_ESCAPE
+        #undef KEY_FN_D
+        #undef KEY_KP_ADD
+        #undef KEY_KP_SUBTRACT
+        #undef KEY_LEFT_CONTROL
+        #undef KEY_NUM_LOCK
+        #undef KEY_TAPE
+        #undef KEY_MINUS
+        #undef KEY_SLASH
+        #undef KEY_SEMICOLON
+        #undef KEY_EQUAL
+        #undef KEY_A
+        #undef KEY_B
+        #undef KEY_C
+        #undef KEY_D
+        #undef KEY_E
+        #undef KEY_F
+        #undef KEY_G
+        #undef KEY_H
+        #undef KEY_I
+        #undef KEY_J
+        #undef KEY_K
+        #undef KEY_L
+        #undef KEY_M
+        #undef KEY_N
+        #undef KEY_O
+        #undef KEY_P
+        #undef KEY_Q
+        #undef KEY_R
+        #undef KEY_S
+        #undef KEY_T
+        #undef KEY_U
+        #undef KEY_V
+        #undef KEY_W
+        #undef KEY_X
+        #undef KEY_Y
+        #undef KEY_Z
+        #undef KEY_BACKSLASH
+        #undef KEY_GRAVE
+        #undef KEY_SPACE
+        #undef KEY_ENTER
+        #undef KEY_TAB
+        #undef KEY_BACKSPACE
+        #undef KEY_INSERT
+        #undef KEY_DELETE
+        #undef KEY_RIGHT
+        #undef KEY_LEFT
+        #undef KEY_DOWN
+        #undef KEY_UP
+        #undef KEY_HOME
+        #undef KEY_END
+        #undef KEY_PAUSE
+        #undef KEY_F1
+        #undef KEY_F2
+        #undef KEY_F3
+        #undef KEY_F4
+        #undef KEY_F5
+        #undef KEY_F6
+        #undef KEY_F7
+        #undef KEY_F8
+        #undef KEY_F9
+        #undef KEY_F10
+        #undef KEY_F11
+        #undef KEY_F12
+        #undef KEY_BACK
+        #undef KEY_MENU
+    #endif
 #endif
+
+#if defined(__linux__)
+//#   define Font Font_
+#endif
+#include "rglfw.c"
+#undef GL_VERSION
+#undef GL_EXTENSIONS
+#undef GL_NUM_EXTENSIONS
+#undef GL_CONTEXT_FLAGS
 
 #if defined(_WIN32)
 //*
@@ -84,53 +182,14 @@
 
 #include "utils.c"
 
+
+//ignore windows for now
 #if defined(__linux__)
-#   undef KEY_CAPS_LOCK
-#   undef KEY_ESCAPE
-#   undef KEY_KP_ADD
-#   undef KEY_KP_SUBTRACT
-#   undef KEY_LEFT_CONTROL
-#   undef KEY_LEFT_CONTROL
-#   undef KEY_NUM_LOCK
-#endif
 #if defined(_WIN32)
 #   define HWND HWND_
 #endif
 #include "rcore.c"
 #undef GLFW_INCLUDE_NONE
-
-
-#if defined(__linux__)
-//  Avoid name conflict with X11
-#   define Font Font_
-#   undef KEY_APOSTROPHE
-#   undef KEY_COMMA
-#   undef KEY_LEFT_CONTROL
-#   undef KEY_KP_SUBTRACT
-#   undef KEY_KP_ADD
-#   undef KEY_CAPS_LOCK
-#   undef KEY_NUM_LOCK
-#   undef KEY_ESCAPE
-#   undef KEY_LEFT_CONTROL
-#   undef KEY_MINUS
-#endif
-
-#if defined (_WIN32)
-#   undef APIENTRY
-typedef WINAPI APIENTRY
-#   define APIENTRY APIENTRY
-#endif
-
-
-#undef GL_VERSION
-#undef GL_EXTENSIONS
-#undef GL_NUM_EXTENSIONS
-#undef GL_CONTEXT_FLAGS
-#include "rglfw.c"
-
-
-//ignore windows for now
-#if defined(__linux__)
 
 #undef _unused
 #include "raudio.c"
