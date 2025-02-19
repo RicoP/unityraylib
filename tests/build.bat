e4d5bc8b3cdda680b0779d090afd436e08c5bfd8
RicoP <RicoP@users.noreply.github.com>
2025-02-19 09:43:20 +0100

    add windows regular build

--- tests/build.bat
+++ tests/build.bat
@@ -9,4 +9,12 @@ cl /EHsc /nologo ^
     ../src/unityraylib.c fps.c ^
     /Fec:unity.exe
 
+cl /EHsc /nologo ^
+    /DPLATFORM_DESKTOP_GLFW ^
+    /I../include/unityraylib/raylib ^
+    /I../include/unityraylib/raylib/external/glfw/include ^
+    ../include/unityraylib/raylib/*.c fps.c ^
+    /Fec:regular.exe ^
+    /link user32.lib gdi32.lib shell32.lib winmm.lib xinput.lib 
+
 DEL *.obj
